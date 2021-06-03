b94b3847d62e6b4cec94b035e9cd4cfbf21bb9e7
张炳彬-Apex <zbb5896@163.com>
2021-06-03 18:32:38 +0800

    提交 dropfile 和 selectgrid 两个组件

---
+++ zhangbingbin.dropfile/zhangbingbin.drop.js
@@ -0,0 +1,269 @@
+(function ($) {
+    //沙箱对外通道
+    $.zhangbingbin = {};
+    //内部文件容器
+    let continers = new Array();
+    //发送的表单
+    let SendFormData = undefined;
+    //配置对象
+    let _config = {
+        //是否支持多文件
+        multiple: undefined,
+        //发送按钮
+        send: undefined,
+        //发送地址
+        url: undefined,
+        //嵌入外部文件流
+        externalFile: undefined,
+        //多通道回调done函数
+        multipath: undefined,
+        //是否同步发送
+        async: undefined,
+        //拖拽或者点击选择文件后回调此函数
+        done: undefined,
+        //发生错误时回调
+        error: undefined,
+        //发送成功后回调此函数
+        success: undefined
+    };
+
+    //判断未定义或者为NULL
+    function IsUnDefinedOrNull(value) {
+        if (value === undefined) {
+            return true;
+        }
+        else if (value === null) {
+            return true;
+        }
+        return false;
+    }
+
+    //查看容器中是否存在同样的文件名的文件流
+    function FileExists(file) {
+        for (let index = 0; index < continers.length; index++)
+            if (file.name == continers[index].name)
+                return false;
+        return true;
+    }
+
+    //初始化发送事件
+    function InitSendEvent() {
+        //必填push的按钮对象和push的地址
+        if (!IsUnDefinedOrNull(_config.send) && !IsUnDefinedOrNull(_config.url)) {
+            _config.send.addEventListener("click", SendEvent);
+        }
+    }
+
+    //发送事件
+    function SendEvent(event) {
+        InitExternal();
+        InitContainerForm();
+        if (!CheckFormData()) return;
+        PushRequest();
+    }
+
+    //外部嵌入文件流加入到表单中
+    function InitExternal() {
+        try {
+            if (!IsUnDefinedOrNull(_config.externalFile) && _config.externalFile instanceof Function) {
+                let externalFile = _config.externalFile();
+                for (let index = 0; index < externalFile.length; index++) {
+                    const element = externalFile[index];
+                    if (element instanceof File)
+                        SendFormData.append(element.name, element.file);
+                    else SendFormData.append("external" + index, element);
+                }
+            }
+        } catch {
+            _config.error({ status: 0, msg: error });
+        }
+    }
+
+    //拖拽容器加入到表单中
+    function InitContainerForm() {
+        try {
+            if (!IsUnDefinedOrNull(continers) && continers.length > 0) {
+                for (let index = 0; index < continers.length; index++) {
+                    const element = continers[index];
+                    SendFormData.append("drop" + index, element);
+                }
+            }
+        } catch (error) {
+            _config.error({ status: 0, msg: error });
+        }
+    }
+
+    //检查游览器是否支持FormData
+    function CheckFormData() {
+        try {
+            if (window.FormData) {
+                if (IsUnDefinedOrNull(SendFormData)) {
+                    SendFormData = new FormData();
+                    return true;
+                }
+                else {
+                    if (SendFormData.keys().length > 0) {
+                        _config.error({ status: 0, msg: "没有可发送的文件!" });
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } catch (error) {
+            _config.error({ status: 0, msg: error });
+        }
+        return false;
+    }
+
+    //浅拷贝配置对象
+    function CopyConfig(config) {
+        for (const key in _config) {
+            if (Object.hasOwnProperty.call(config, key)) {
+                _config[key] = config[key];
+            }
+        }
+    }
+
+    //载入容器
+    function PushContainers(obj) {
+        if (!IsUnDefinedOrNull(_config.multiple)) {
+            continers.push(obj);
+        }
+        else {
+            continers[0] = obj;
+        }
+    }
+
+    //推出请求
+    function PushRequest() {
+        var httpReq = new XMLHttpRequest();
+        if (_config.async == null) _config.async = true;
+        httpReq.open("post", _config.url, _config.async);
+        httpReq.onreadystatechange = function () {
+            if (httpReq.readyState == 4) {
+                if (httpReq.status == 200) {
+                    _config.success(JSON.stringify(eval('(' + httpReq.responseText + ')')));
+                }
+                else {
+                    _config.error({
+                        status: httpReq.status,
+                        msg: httpReq.responseText
+                    });
+                }
+            }
+        }
+        httpReq.send(SendFormData);
+    }
+
+    //初始化拖拽组件的事件
+    function InitDropEvent(element) {
+        element.addEventListener("click", function (e) {
+            let inputFile = document.createElement('input');
+            inputFile.setAttribute('type', 'file');
+            inputFile.setAttribute("style", 'visibility:hidden;');
+            if (_config.multiple)
+                inputFile.setAttribute("multiple", 'multiple');
+            inputFile.addEventListener("change", function (e) {
+                for (let i = 0; i < this.files.length; i++) {
+                    const file = this.files[i];
+                    if (FileExists(file)) {
+                        PushContainers(file);
+                    }
+                }
+                if (continers.length > 0)
+                    _config.done(continers);
+                this.remove();
+            }, false);
+            document.body.appendChild(inputFile);
+            inputFile.click();
+        }, false);
+        element.addEventListener("dragenter", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+        }, false);
+        element.addEventListener("dragover", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+        }, false);
+        element.addEventListener("dragleave", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+        }, false);
+        element.addEventListener("drop", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            try {
+                //传输的数据
+                let Stream = e.dataTransfer;
+                //接收的文件数量
+                let DealFileCount = 0;
+                //数据中所有文件的数量
+                let FilesLength = Stream.files.length;
+                //数据中的项目
+                let Items = Stream.items;
+                //确认接收完成
+                function CheckDropFinish() {
+                    if (DealFileCount === FilesLength - 1) {
+                        getDropFileCallBack(continers);
+                    }
+                    DealFileCount++;
+                }
+                //WebKit内核游览器
+                if (Items !== undefined) {
+                    for (let i = 0; i < Items.length; i++) {
+                        const item = Items[i];
+                        if (item.kind === "file" && item.webkitGetAsEntry().isFile) {
+                            let file = item.getAsFile();
+                            if (FileExists(file)) PushContainers(file);
+                            else _config.error({ status: 0, msg: "已存在该文件" + file.name });
+                        }
+                    }
+                }
+                //IE游览器
+                else {
+                    for (let i = 0; i < FilesLength; i++) {
+                        const file = Items[i];
+                        if (FileExists(file)) {
+                            if (file.type) {
+                                PushContainers(file);
+                                CheckDropFinish();
+                            } else {
+                                try {
+                                    var fileReader = new FileReader();
+                                    //写入数据地址
+                                    fileReader.readAsDataURL(file.slice(0, 3));
+                                    //监听读取
+                                    fileReader.addEventListener('load', function (e) {
+                                        PushContainers(file);
+                                        CheckDropFinish();
+                                    }, false);
+                                    //监听读取失败
+                                    fileReader.addEventListener('error', function (error) {
+                                        _config.error({ status: 0, msg: error });
+                                        CheckDropFinish();
+                                    }, false);
+                                } catch (error) {
+                                    _config.error({ status: 0, msg: error });
+                                    CheckDropFinish();
+                                }
+                            }
+                        }
+                        else _config.error({ status: 0, msg: "已存在该文件" + file.name });
+                    }
+                }
+            } catch (error) {
+                _config.error({ status: 0, msg: error });
+            }
+            _config.done(continers);
+        }, false);
+    }
+
+    var _dropComponent = function (element, config) {
+        if (!CheckFormData()) return;
+        CopyConfig(config);
+        InitDropEvent(element);
+        InitSendEvent();
+    };
+
+    $.zhangbingbin.drop = _dropComponent;
+}(window));
---
+++ zhangbingbin.selectgrid/zhangbingbin.selectgrid.js
@@ -0,0 +1,220 @@
+(function (win) {
+    win.zhangbingbin = {};
+    win.zhangbingbin.select = {};
+    function selectgrid(cols, data, callback) {
+        var tempData = data;
+        var nFocus = false;
+        var selectS = document.querySelectorAll(".dbUI-select");
+        Array.from(selectS).forEach(item => {
+            let mainWidth = item.getAttribute("width") || "180";
+            let mainHeight = item.getAttribute("height") || "25";
+            let inputWidth = (Number(mainWidth) - 35) + "";
+            let viewContent = "";
+            let cursorContent = {};
+            item.style = `width:${mainWidth}px;height:${mainHeight}px;`;
+            let input = document.createElement("input");
+            input.className = "dbUI-select-input",
+                input.type = "text",
+                input.readOnly = true,
+                input.style = `width:${inputWidth}px;`;
+            item.appendChild(input);
+            let font = document.createElement("div");
+            font.className = "dbUI-select-font";
+            item.appendChild(font);
+            let icon = document.createElement("div");
+            icon.className = "dbUI-select-btn", icon.innerText = "▾";
+            item.appendChild(icon);
+            let container = document.createElement("div");
+            container.className = "dbUI-select-container";
+            item.appendChild(container);
+            icon.addEventListener("click", function (e) {
+                if (container.classList.contains("show")) {
+                    container.classList.remove("show");
+                }
+                else container.classList.add("show");
+                input.click();
+                input.focus();
+            });
+            let tableHead = document.createElement("div");
+            tableHead.className = "dbUI-select-container-content-thead";
+            container.appendChild(tableHead);
+            let content = document.createElement("div");
+            content.className = "dbUI-select-container-content";
+            content.style = `min-width:${mainWidth}px;`;
+            container.appendChild(content);
+            let help = document.createElement("div");
+            help.className = "dbUI-select-container-help";
+            container.appendChild(help);
+            let helpinput = document.createElement("input");
+            helpinput.type = "button", helpinput.value = "x";
+            help.appendChild(helpinput);
+            let helpdiv = document.createElement("div");
+            helpdiv.innerText = "⊿";
+            help.appendChild(helpdiv);
+            let containerDrop = false;
+            let currX = 0;
+            let currY = 0;
+            let currW = 0;
+            let currH = 0;
+            helpdiv.addEventListener("mousedown", function (e) {
+                containerDrop = true;
+                currX = e.clientX;
+                currY = e.clientY;
+                currW = content.clientWidth;
+                currH = content.clientHeight;
+            }, true);
+            document.addEventListener("mousemove", function (e) {
+                if (containerDrop) {
+                    let diffX = e.clientX - currX;
+                    let diffY = e.clientY - currY;
+                    container.style = "";
+                    content.setAttribute("style", `width:${(currW + diffX)}px;height:${(currH + diffY)}px;min-width:${mainWidth}px;`);
+                }
+            }, true);
+            document.addEventListener("mouseup", function (e) {
+                containerDrop = false;
+            }, true);
+            helpinput.addEventListener("click", function (e) {
+                if (container.classList.contains("show")) {
+                    container.classList.remove("show");
+                }
+                else container.classList.add("show");
+            });
+            let table = document.createElement("table");
+            table.setAttribute("cellspacing", "0"), table.setAttribute("cellpadding", "0");
+            content.appendChild(table);
+            let thead = document.createElement("thead");
+            table.appendChild(thead);
+            let tbody = document.createElement("tbody");
+            table.appendChild(tbody);
+            refreshGrid();
+            var loadFilter = "";
+            item.addEventListener("click", function (e) { nFocus = true; }, true);
+            document.addEventListener("mousedown", function (e) {
+                if (!e.target.offsetParent
+                    || !e.target.offsetParent == item
+                    || !e.target.offsetParent == container) {
+                    nFocus = false;
+                    font.innerHTML = viewContent;
+                    loadFilter = "";
+                    if (container.classList.contains("show")) {
+                        container.classList.remove("show");
+                    }
+                }
+            }, true);
+            input.addEventListener("keydown", function (e) {
+                if (nFocus) {
+                    if (e.keyCode == 8) {
+                        let temp = loadFilter.substring(0, loadFilter.length - 1);
+                        filterFont(temp);
+                    } else {
+                        let value = String.fromCharCode(e.keyCode);
+                        if (/[0-9|A-Z|a-z]/.test(value)) {
+                            let temp = loadFilter + value;
+                            filterFont(temp);
+                        }
+                    }
+                }
+            });
+            document.addEventListener("keydown", function (e) {
+                if (nFocus) {
+                    if (e.keyCode == 13) {
+                        cursorContent.click();
+                    }
+                    if (e.keyCode == 38) {
+                        let prev = cursorContent.previousElementSibling;
+                        if (prev) {
+                            refreshCursor(prev);
+                            let target = prev.querySelector("td[c-key]");
+                            filterFont(target.innerText, loadFilter);
+                        }
+                    }
+                    if (e.keyCode == 40) {
+                        let next = cursorContent.nextElementSibling;
+                        if (next) {
+                            refreshCursor(next);
+                            let target = next.querySelector("td[c-key]");
+                            filterFont(target.innerText, loadFilter);
+                        }
+                    }
+                }
+            }, true);
+            function filterFont(temp, temp2) {
+                let col = cols.filter(x => x.key);
+                if (col.length > 0) {
+                    eval("var filterData = data.filter(x => /^" + temp.toLowerCase() + "/.test(x.col1.toLowerCase()));")
+                    if (filterData.length > 0) {
+                        if (temp2 == undefined) {
+                            tempData = filterData;
+                            refreshGrid();
+                            loadFilter = temp;
+                        }
+                        let showView = callback(filterData[0]);
+                        viewContent = showView;
+                        font.innerHTML = showView.substring(0, (temp2 || temp).length) + "<label>" + showView.substring((temp2 || temp).length) + "</label>";
+                        if (temp === "") font.innerHTML = showView;
+                        if (!container.classList.contains("show"))
+                            container.classList.add("show");
+                    }
+                }
+            }
+            function refreshGrid() {
+                thead.innerHTML = "";
+                tbody.innerHTML = "";
+                tableHead.innerHTML = "";
+                Array.from(cols).forEach(col => {
+                    let th = document.createElement("th");
+                    th.setAttribute("width", col.width);
+                    thead.appendChild(th);
+                    let colDiv = document.createElement("div");
+                    colDiv.className = "col", colDiv.innerText = col.name;
+                    colDiv.style = col.width ? `flex-basis:${(col.width - 1)}px;` : "flex-grow:1;";
+                    tableHead.appendChild(colDiv);
+                });
+                let first = true;
+                Array.from(tempData).forEach(dta => {
+                    let tr = document.createElement("tr");
+                    if (first) {
+                        tr.classList.add("show");
+                        first = false;
+                        cursorContent = tr;
+                    }
+                    tbody.appendChild(tr);
+                    tr.addEventListener("mouseenter", function (e) {
+                        refreshCursor(this);
+                    }, true);
+                    tr.addEventListener("click", function (e) {
+                        let target = this.querySelector("td[c-key]");
+                        if (target) {
+                            filterFont(target.innerText);
+                            nFocus = false;
+                            font.innerHTML = viewContent;
+                            loadFilter = "";
+                        }
+                        if (container.classList.contains("show")) {
+                            container.classList.remove("show");
+                        }
+                    });
+                    Array.from(cols).forEach(col => {
+                        let key = col.field;
+                        let isKey = col.key || false;
+                        let td = document.createElement("td");
+                        td.innerText = dta[key];
+                        tr.appendChild(td);
+                        if (isKey) td.setAttribute("c-key", col.field);
+                    });
+                });
+            }
+            function refreshCursor(tr) {
+                Array.from(tr.parentNode.childNodes).forEach(x => {
+                    if (x.classList.contains("show")) {
+                        x.classList.remove("show");
+                    }
+                });
+                tr.classList.add("show");
+                cursorContent = tr;
+            }
+        });
+    }
+    win.zhangbingbin.selectgrid = selectgrid;
+}(window));
